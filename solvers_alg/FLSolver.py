3ed20c83b6d2f4371e38e763aecde40292894680
nff-03 <nour_fayadh2003@yahoo.com>
2025-10-15 12:38:42 -0400

    Updates to initial solver files

--- solvers_alg/FLSolver.py
+++ solvers_alg/FLSolver.py
@@ -3,16 +3,18 @@ from solvers_alg.solver import Solver
 
 class FLSolver(Solver):
     def __init__(self):
+        self.name = None
+        self.solutionValue = 0
         self.selectedFacilities = []
 
     def getName(self):
-        raise NotImplementedError
+        return self.name
 
     def solve(self, graph, n, k):
         raise NotImplementedError
 
     def getSolutionValue(self):
-        raise NotImplementedError
+        return self.solutionValue
 
     def getSelectedFacilites(self):
-        raise NotImplementedError
+        return self.selectedFacilities
