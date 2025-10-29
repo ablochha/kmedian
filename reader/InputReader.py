5ded0ae59c1128032264668b4768b6d0fc5bcc82
nff-03 <nour_fayadh2003@yahoo.com>
2025-10-28 21:41:48 -0400

    Added InputReader abstract, and added formatted test files for pmed, special, and tsplib

---
+++ reader/InputReader.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+class InputReader(ABC):
+    @abstractmethod
+    def __init__(self):
+        self._next = None
+
+    @abstractmethod
+    def set_next(self, next):
+        self._next = next
+        return next
+    
+    @abstractmethod
+    def read(self, input):
+        if(self.canRead(input)):
+            return self.parse(input)
+        elif(self._next is not None):
+            return self._next.read(input)
+        else:
+            raise Exception(f"Unsupported file format: {input}")
+        
+
+    @abstractmethod
+    def canRead(self, input):
+        pass
+
+    @abstractmethod
+    def parse(self, input):
+        pass
